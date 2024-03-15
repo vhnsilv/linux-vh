55e1fcb4ab9bc2326980f1b4d7a60b28ffe62ade
Victor Hugo <137303782+vhnsilv@users.noreply.github.com>
2024-03-15 09:44:00 -0300

    Update script_webserver.sh

--- script_webserver.sh
+++ script_webserver.sh
@@ -20,7 +20,7 @@ wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.z
 echo "Inserindo aplicação..."
 
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html/
 
 echo "Finalizado!!!"
